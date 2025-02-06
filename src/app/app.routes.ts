23b966883826b274464635daaf9053364e1e1b1f
Samir Jose Giil <144859085+SamirJoseGil@users.noreply.github.com>
2025-02-06 18:05:45 -0500

    Initial project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+
+import { HomeComponent } from './pages/home/home.component';
+import { SignupComponent } from './pages/signup/signup.component';
+
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+
+    { path: 'home', component: HomeComponent },
+    { path: 'signup', component: SignupComponent },
+
+
+
+
+
+
+
+
+
+    
+];
